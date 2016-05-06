fd6f492ec724057b614db86440753538d7d232c4
TOMOKO HIRATA <tomoko.t.523@outlook.jp>
2016-05-06 18:00:33 +0900

    add table

--- 6/output_data.php
+++ 6/output_data.php
@@ -15,13 +15,13 @@ if (isset($_POST['arr'])) {
 }
 
 if ($name != null and $mail != null) {
-  array_unshift($arr, $name, $mail);
-  $file = fopen('data/data.csv', 'a'); //add:追記
+    array_unshift($arr, $name, $mail);
+    $file = fopen('data/data.csv', 'a'); //add:追記
   flock($file, LOCK_EX);//ロック開始
   if ($file) {
       fputcsv($file, $arr);
   }
-  flock($file, LOCK_UN);//ロック解除
+    flock($file, LOCK_UN);//ロック解除
   fclose($file);
 }
 
@@ -60,16 +60,56 @@ if ($name != null and $mail != null) {
 <head>
     <meta charset="UTF-8">
     <title>アンケート結果</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/mapcoler.css">
 </head>
 
 <body>
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#page_map" data-toggle="tab">MAP</a></li>
+    <li><a href="#page_table" data-toggle="tab">Table</a></li>
+  </ul>
+  <div id="myTabContent" class="tab-content">
+  <div id="page_map" class="tab-pane fade in active">
     <div id="main">
         <div id="map"></div>
     </div>
-    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="./js/jquery.japan-map.min.js"></script>
-    <script id="mainsrc" src="./js/main.js"></script>
+  </div>
+  <div id="page_table" class="tab-pane fade">
+    <div class="container">
+      <table class="table table-striped">
+      <thead>
+        <tr>
+          <th></th>
+          <th>お名前</th>
+          <th>MAIL</th>
+          <th>都道府県No</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $filepath = './data/data.csv';
+        $prefecture = [];
+        $data = file($filepath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($data as $line) {
+            $tmp = explode(',', $line);
+            echo '<tr>';
+            echo '<td></td>';
+            echo "<td>$tmp[0]</td>";
+            echo "<td>$tmp[1]</td>";
+            unset($tmp[0], $tmp[1]);
+            $area = implode(',', $tmp);
+            echo "<td>$area</td>";
+            echo '</tr>';
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="./js/jquery.japan-map.min.js"></script>
+<script id="mainsrc" src="./js/main.js"></script>
 </body>
-
 </html>
