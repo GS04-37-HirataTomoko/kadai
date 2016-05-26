818156dd27eaf5810840bf5aa96874ed51664966
TOMOKO HIRATA <tomoko.t.523@outlook.jp>
2016-05-27 01:26:07 +0900

    editor_css finish

--- 9/script/ng_main.js
+++ 9/script/ng_main.js
@@ -4,21 +4,26 @@ angular.module("MyApp", ["ngSanitize", "ui.ace"])
         $locationProvider.html5Mode(true);
     }])
     .run(["$rootScope", function($scope) {
-        $scope.edit_js = "";
-        $scope.edit_css = "section{background-color: #e6e6fa;}#aaa{color: red;}";
+        $scope.edit_js = "<section></section>";
+        $scope.edit_css = "section{background-color: #e6e6fa;box-shadow:5px 5px 0px -2px;margin:10px;}#aaa{color: red;}";
     }])
     .controller('LoginDataCtrl', function($scope, $location) {
         this.guest = false;
         this.lguser = true;
         this.paramName = "name";
         this.username = $location.search()[this.paramName];
-        if(angular.isUndefined(this.username)){
-          this.username = 'Guest';
-          this.guest = true;
-          this.lguser = false;
+        if (angular.isUndefined(this.username)) {
+            this.username = 'Guest';
+            this.guest = true;
+            this.lguser = false;
         };
         // this.click_logout = function(){
         //   console.log("ログアウト");
         //   $window.location.href = '../php/logout.php';
         // };
+    })
+    .filter('unsafe', function($sce) {
+        return function(val) {
+            return $sce.trustAsHtml(val);
+        };
     });
